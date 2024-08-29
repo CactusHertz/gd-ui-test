379dee37dc5a5cc282fad8402fe99a2a02011785
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-08-28 22:49:39 -0700

    Add drop down

--- ui.gd
+++ ui.gd
@@ -2,5 +2,10 @@ extends CanvasLayer
 
 @onready var fps = $Fps
 
+@onready var square = $Square
+
+func _ready():
+	pass #square.
+
 func _on_check_button_toggled(toggled_on):
 	fps.visible = toggled_on
