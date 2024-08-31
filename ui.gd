4f56b2b6c1933dea9379b8f35c98d79434dc2782
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-08-31 00:08:59 -0700

    Add dropdown options

--- ui.gd
+++ ui.gd
@@ -9,3 +9,13 @@ func _ready():
 
 func _on_check_button_toggled(toggled_on):
 	fps.visible = toggled_on
+
+
+func _on_option_button_item_selected(index):
+	print(index)
+	match index:
+		0:
+			square.scale = Vector2(2,2)
+		1:
+			square.scale = Vector2(1,1)
+
